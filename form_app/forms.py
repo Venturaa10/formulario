b5ed66109eed07867ce21885afcbea836b8419ec
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-06-23 11:47:03 -0300

    Criando e Salvando o cliente cadastro da forma correta no modelo de 'Cliente' no admin.

--- form_app/forms.py
+++ form_app/forms.py
@@ -1,56 +1,18 @@
 # Arquivo responsavél por receber as informações do usuario
 from django import forms 
+from form_app.models import Cliente
 
-class CadastroUser(forms.Form):
-    nome_user = forms.CharField(
-        # Parametros aqui
-        label='Nome de Usuario',
-        required=True,
-        max_length=40,
-        widget=forms.TextInput(
-            attrs={
-                'placeholder':'Seu nome',
-                'class': 'form-control',
-                   }
-        )
+class ClienteForm(forms.ModelForm):
+    class Meta:
+        model = Cliente
+        fields = ['nome', 'sobrenome', 'idade', 'email', 'telefone', 'comentario', 'ativo']
+        widgets = {
+            'nome': forms.TextInput(attrs={'placeholder': 'Seu nome'}),
+            'sobrenome': forms.TextInput(attrs={'placeholder': 'Seu sobrenome'}),
+            'idade': forms.NumberInput(attrs={'placeholder': 'Sua idade'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'SeuEmail@caminho.com'}),
+            'telefone': forms.TextInput(),
+            'comentario': forms.Textarea(attrs={'placeholder': 'Sugestões'}),
+            'ativo': forms.CheckboxInput(),
+        }
 
-    )
-
-    sobrenome_user = forms.CharField(
-        # Parametros aqui
-        label='Sobrenome',
-        required=True,
-        max_length=40,
-        widget=forms.TextInput(
-            attrs={
-                'placeholder':'Seu nome',
-                'class': 'form-control',
-                   }
-        )
-    )
-
-    email_user = forms.EmailField(
-        label='Email',
-        required=True, # Torna o preenchimento desse campo obrigatorio
-        max_length= 100,
-        widget=forms.EmailInput(
-            # Estilizando os labels do HTML através do proprio atributo do objeto
-            attrs={
-                'class': 'form-control', 
-                'placeholder': 'seuemail@tipo.com'
-            }
-        )
-    )
-
-    comentario_user = forms.CharField(
-        label='Comentario',
-        required=True, # Torna o preenchimento desse campo obrigatorio
-        max_length= 500,
-        widget=forms.TextInput(
-            # Estilizando os labels do HTML através do proprio atributo do objeto
-            attrs={
-                'class': 'form-control', 
-                'placeholder': 'Deixe um comentario'
-            }
-        )
-    )
--- form_app/views.py
+++ form_app/views.py
@@ -1,32 +1,37 @@
 from django.shortcuts import render, redirect
-from form_app.forms import CadastroUser
+from form_app.forms import ClienteForm
 from django.contrib.auth.models import User
+from form_app.models import Cliente
 
 # Create your views here.
 
 def index(request):
     '''Exibe a pagina de cadastro'''
-    form = CadastroUser()
+    form = ClienteForm()
 
     return render(request,'index.html', {'form': form})
 
 def cadastro(request):
-    form = CadastroUser(request.POST) #Instanciando o objeto CadastroUser em uma variavel 
+    form = ClienteForm(request.POST) #Instanciando o objeto CadastroUser em uma variavel 
 
     if form.is_valid():
-        nome = form['nome_user'].value()
-        sobrenome = form['sobrenome_user'].value()
-        email = form['email_user'].value()
-        comentario = form['comentario_user'].value()
+        nome_form = form['nome'].value()
+        sobrenome_form = form['sobrenome'].value()
+        idade_form = form['idade'].value()
+        email_form = form['email'].value()
+        telefone_form = form['telefone'].value()
+        comentario_form = form['comentario'].value()
+        ativo_form = form['ativo'].value()
 
-        cadastro_usuario = User.objects.create_user(
-            username=nome,
-            email=email,
+        cliente = Cliente.objects.create(
+            nome=nome_form,
+            sobrenome=sobrenome_form,
+            idade=idade_form,
+            email=email_form,
+            telefone=telefone_form,
+            comentario=comentario_form,
+            ativo=ativo_form,
         )
-        
-        cadastro_usuario.save() #Cadastrando / Salvando o usuario no banco de dados
+        cliente.save() #Cadastrando / Salvando o usuario no banco de dados
             
         return redirect('index')
-    
-def valida_nome(request):
-    pass
